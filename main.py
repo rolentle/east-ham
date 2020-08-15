cb55594f5f9beb913c59131f05fa39a34f119538
Rolen T. Le <rolentle@gmail.com>
2020-08-14 20:50:26 -0400

    extract corpus and add recursive logic

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
 from nltk.tokenize.sonority_sequencing import SyllableTokenizer
 import random
+import re
 
-
-class EnglishTownNameGenerator():
-    def __init__(self, file_name="./town_names.txt", tokenizer=SyllableTokenizer):
+class Corpus():
+    def __init__(self, tokenizer=SyllableTokenizer):
+        self.file_name = "./town_names.txt"
         self.tokenizer = tokenizer()
-        self.file_name = file_name
-        self.corpus = self.generate_corpus()
-
-    def generate_times(self, n=10):
-        return [self.generate() for _ in range(n)]
-
-    def generate(self):
-        town_name = []
-        current_syllable = 0
-        random_town = random.choice(self.corpus)
-        while len(random_town) > current_syllable:
-            syllable = random_town[current_syllable]
-            town_name.append(syllable)
-            current_syllable += 1
-            random_town = random.choice(self.corpus)
-
-        return "".join(town_name)
 
     def generate_corpus(self):
         f = open(self.file_name, "r")
@@ -29,6 +13,31 @@ class EnglishTownNameGenerator():
 
     def tokenize(self, name):
         return self.tokenizer.tokenize(name.strip())
+
+class EnglishTownNameGenerator():
+    def __init__(self, corpus=Corpus):
+        self.corpus = Corpus().generate_corpus()
+
+    def generate_times(self, n=10):
+        return [self.generate() for _ in range(n)]
+
+    def generate(self, recursive=True):
+        town_name_syllables = []
+        current_syllable_index = 0
+        random_town = random.choice(self.corpus)
+        while len(random_town) > current_syllable_index:
+            syllable = random_town[current_syllable_index]
+            town_name_syllables.append(syllable)
+            if re.search("\W+", syllable) and recursive:
+                town_name_syllables.append(self.generate(recursive=False))
+            elif syllable == random_town[-1]:
+                break
+
+            current_syllable_index += 1
+            random_town = random.choice(self.corpus)
+
+        return "".join(town_name_syllables)
+
 
 
 if __name__ == "__main__":
