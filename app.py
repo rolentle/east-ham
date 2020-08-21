0e0dd85413c7b4e3879ca0d62e797135ffe3d52a
Rolen T. Le <rolentle@gmail.com>
2020-08-20 21:31:42 -0400

    add welsh names

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def names():
     town_name_corpus = Corpus(file_name="./town_names.txt")
+    welsh_town_name_corpus = Corpus(file_name="./welsh_town_names.txt")
     npc_name_corpus = Corpus(file_name="./names.txt")
     names = [ name for name in EnglishTownNameGenerator(town_name_corpus).generate_times(20)]
+    welsh_names = [ welsh_name for welsh_name in EnglishTownNameGenerator(welsh_town_name_corpus).generate_times(20)]
     npc_names = [ npc_name for npc_name in EnglishTownNameGenerator(npc_name_corpus).generate_times(20)]
-    return render_template("names.html", names=names, npc_names=npc_names)
+    return render_template("names.html", names=names, welsh_names=welsh_names,npc_names=npc_names)
 
 if __name__ == "__main__":
     app.run()
