28c3b2506e10b14e66f58116ace5309a68479900
Rolen T. Le <rolentle@gmail.com>
2020-08-20 21:24:39 -0400

    wip

---
+++ lineage.py
@@ -0,0 +1,31 @@
+import simpy
+env = simpy.Environment()
+
+class Person:
+    def __init__(self,name, age, gender):
+        self.name = name
+        self._age = age
+        self.gender = gender
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        self._age = value
+
+
+def greet(env, person):
+    while True:
+        name = person.name
+        age = person.age
+        msg = f"My name is {name}, I am {age} years old"
+        print(msg)
+        person.age += 1
+        yield env.timeout(1)
+
+if __name__ == "__main__":
+    person = Person("Rolen", 33, "male")
+    env.process(greet(env, person))
+    env.run(until=5)
